a5247a9a7830d4b083b3262120bed3652d66ee59
Paige Jones <hello@paigejones.me>
2022-06-20 16:16:49 +0100

    feat: add primary menu

--- resources/views/_hero.blade.php
+++ resources/views/_hero.blade.php
@@ -1,6 +1,10 @@
 <div>
     <div class="h-40 bg-green-500"></div>
-    <div class="bg-white flex px-6 py-3">
-        navigation
+    <div class="bg-white flex justify-center px-6 py-3 space-x-3">
+        @foreach(getMenuItems('primary') as $item)
+            <a class="font-bold text-gray-700 text-sm uppercase hover:text-blue-500" href="{{ $item['slug'] }}">
+                {{ $item['title'] }}
+            </a>
+        @endforeach
     </div>
 </div>
